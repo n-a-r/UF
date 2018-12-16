a02cdfa4aab5873c9eec08b3114e108dc6712481
nat <rosenbloom.n@husky.neu.edu>
2018-12-16 15:06:54 -0500

    Initial commit

---
+++ uf.h
@@ -0,0 +1,12 @@
+struct uf_;
+
+typedef struct uf_ UF;
+
+// creates a new union-find with n sets, each with the integers 0 to n-1 in them
+UF* uf_init(int n);
+
+// if two elements are in the same set, find will return the same integer for both
+int uf_find(UF* s, int x);
+
+// unions the set of the two elements together
+void uf_union(UF* s, int x, int y);
